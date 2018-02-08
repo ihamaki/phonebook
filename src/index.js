01f6b426ae83114a7c398da6f3229fd84f9ac3cf
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-02-08 22:01:59 +0200

    exercise 3.2 & 3.3

--- src/index.js
+++ src/index.js
@@ -24,8 +24,31 @@ let persons = [
   }
 ]
 
+const generateInfo = () => {
+  return (
+    `<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p>
+     <p>${Date()}</p>`
+  )
+}
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/info', (req, res) => {
+  const info = generateInfo()
+  res.send(info)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
 })
 
 const PORT = 3001
