f9819b1b94b56211c5f484d8ed6ccad85180c451
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-02-09 15:25:35 +0200

    initiate app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body)
@@ -91,7 +93,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
