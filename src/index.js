3307f9e180878b373dadca667427a87a04965707
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2018-02-10 23:36:43 +0200

    exercise 3.13-3.16 & 3.18

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(bodyParser.json())
-morgan.token('data', (req, res) => {
-  return JSON.stringify(req.body)
-})
+morgan.token('data', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method, :url, :data, :status, :res[content-length] - :response-time ms'))
 app.use(cors())
 app.use(express.static('build'))
@@ -46,8 +45,21 @@ const getRandom = () => {
   return Math.floor(Math.random() * Math.floor(100000))
 }
 
+const formatPerson = (person) => {
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  }
+}
+
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person
+    .find({})
+    .then(persons => {
+      res.json(persons.map(formatPerson))
+    })
+    .catch(error => console.log(error))
 })
 
 app.get('/api/info', (req, res) => {
@@ -63,35 +75,49 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({ error: 'name or number missing' })
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return res.status(400).json({ error: 'name already in phonebook' })
-  }
+  // if (persons.find(person => person.name === body.name)) {
+  //   return res.status(400).json({ error: 'name already in phonebook' })
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id: getRandom()
-  }
+  })
 
-  persons = persons.concat(person)
-  res.json(person)
+  person
+    .save()
+    .then(savedPerson => {
+      res.json(formatPerson(savedPerson))
+    })
+    .catch(error => console.log(error))
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person
+    .findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(formatPerson(person))
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const PORT = process.env.PORT || 3001
